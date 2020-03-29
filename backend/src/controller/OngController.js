173dd19ed906e2aa88785665a92c214310cf5891
xxbielxd <gabrielvictorxd@gmail.com>
2020-03-28 22:13:36 -0300

    front end finalizado

--- backend/src/controller/OngController.js
+++ backend/src/controller/OngController.js
@@ -10,7 +10,7 @@ module.exports = {
     const { name, email, whatsapp, city, uf } = request.body;  
     const id = crypto.randomBytes(4).toString('HEX');
 
-   await connection('ongs').insert({
+    await connection('ongs').insert({
       id,
       name,
       email,
@@ -18,6 +18,7 @@ module.exports = {
       city,
       uf,
     })
+    
 
     return response.json({id});
   }
